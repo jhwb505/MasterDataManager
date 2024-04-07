6a74ca9e5bf960dfb90fb5bfb8774a236569ce4f
jhwb505 <jhwb505@gmail.com>
2024-04-07 20:51:18 +0900

    Initial Commit

---
+++ src/test/java/com/dddca/masterdatamanager/MasterDataManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dddca.masterdatamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterDataManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
